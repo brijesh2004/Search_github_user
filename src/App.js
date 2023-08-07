fb260671d93e66367362afd5eb4ef26daf06e244
brijesh2004 <brskumar0102@gmail.com>
2023-08-07 21:43:42 +0530

    github user

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'; // Import React and the necessary hooks
 import './App.css';
-
 function App() {
+  const [username, setUsername] = useState("brijesh");
+  const [data, setData] = useState({});
+
+  const searchUser = async () => {
+    try {
+      const response = await fetch(`https://api.github.com/search/users?q=${username}`);
+      const responseData = await response.json();
+      setData(responseData.items);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+
+  useEffect(() => {
+    searchUser();
+  } , [username]); // Empty dependency array to run the effect once on initial render
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='page'>
+    <center><h2>Search the Github Users with the user name</h2></center>
+      <input className='input_field' type="text" placeholder='Search the user' name='user' value={username} onChange={(e)=>setUsername(e.target.value)}/>
+     
+      <div className='users'>
+      {
+        
+        Array.isArray(data)&&data.map((elem ,index) => (
+          <div className='user_box'>
+           {/* <p>username : - {elem.login}</p> */}
+           <img src={elem.avatar_url} width={80} height={80} /> <br />
+           {elem.login} <br />
+           {/* <p>followers: {elem.followers}</p> */}
+          <button><a href={elem.html_url} style={{textDecoration:'none'}}>Visit Profile</a></button> 
+          </div>
+        ))
+      }
+      </div>
     </div>
   );
 }
